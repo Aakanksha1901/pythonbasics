4692e55fdb78835cdec1efd50034f14c8b308f9b
Aakanksha1901 <124653368+Aakanksha1901@users.noreply.github.com>
2026-01-04 23:32:43 +0530

    Add files via upload

---
+++ copylist.py
@@ -0,0 +1,8 @@
+a = [1,2,3]
+b =a.copy()#shallow copy
+c =list(a)
+d = a[:]#slicing copy
+
+print(b)
+print(c)
+print(d)
---
+++ createlist.py
@@ -0,0 +1,12 @@
+list1 =[ ]   #emptylist
+list2 =[1,2,3,4,5]   #with integers
+list3 =["apple","banana"]  #with strings
+list4 =[1,2.5,"Hello",[5,6]]  #with nested list
+list5 = list((1,2,3))   #using list() constructor
+
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
